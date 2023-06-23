f4f239f9f47912376059aca045755910c7b42b63
carrottechplus <blohzzang@gmail.com>
2023-06-23 13:22:09 +0900

    fontawesome 연결

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import pic1 from './img/pic1.jpg';
-import pic2 from './img/pic2.jpg';
-import pic3 from './img/pic3.jpg';
-
 import './scss/style.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
 
 function App() {
-	const path = process.env.PUBLIC_URL; // public 폴더까지의 절대 경로값
 	return (
 		<>
-			<div>
-				<h1>src 폴더 안쪽에서 이미지 import</h1>
-				<img src={pic1} alt='pic1' />
-				<img src={pic2} alt='pic2' />
-				<img src={pic3} alt='pic3' />
-			</div>
-			<div>
-				<h1>public 폴더 안쪽에서 이미지 경로 연결</h1>
-				<img src={path + '/img/pic1.jpg'} alt='pic1' />
-				<img src={path + '/img/pic2.jpg'} alt='pic2' />
-				<img src={path + '/img/pic3.jpg'} alt='pic3' />
-			</div>
+			<FontAwesomeIcon icon={faHouse} />
+			<FontAwesomeIcon icon={faEnvelope} />
+			<FontAwesomeIcon icon={faThumbsUp} />
 		</>
 	);
 }
