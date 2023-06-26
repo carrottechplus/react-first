0068d007992a0a61ff8a6684d8e5165f23781af0
carrottechplus <blohzzang@gmail.com>
2023-06-26 11:33:26 +0900

    style 모션 처리

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import Popup from './Popup';
 import './scss/style.scss';
 import { useState } from 'react';
 
-// 팝업열기 버튼 클릭 시 외부 컴포넌트인 popup 컴포넌트 보이게
-// 팝업 컴포넌트 안쪽에 닫기 버튼을 만들어서 해당 닫기 버튼 클릭시 팝업제거
-// 팝업 컴포넌트 안쪽에 숫자 출력하고 그 밑에 증가, 감소 버튼을 만들어서 해당 버튼 클릭시 숫자가 증가, 감소 출력되도록 처리
-
+// 좌우버튼 클릭 시 45도씩 왼쪽, 오른쪽 회전되게 모션 처리
 function App() {
-	const [Open, setOpen] = useState(false);
+	const [Trans, setTrans] = useState('rotate(0deg)');
 	return (
-		<>
-			<main>
-				<h1>test</h1>
-				<button
-					onClick={() => {
-						setOpen(true);
-					}}
-				>
-					팝업 열기
-				</button>
-				{Open && <Popup setOpen={setOpen} />}
-			</main>
-		</>
+		<div className='wrap'>
+			<button
+				onClick={() => {
+					setTrans('rotate(-45deg)');
+				}}
+			>
+				왼쪽으로 회전
+			</button>
+			<button
+				onClick={() => {
+					setTrans('rotate(45deg)');
+				}}
+			>
+				오른쪽으로 회전
+			</button>
+			<article style={{ transform: Trans }}></article>
+		</div>
 	);
 }
 
--- src/Popup.js
+++ src/Popup.js
@@ -1,7 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+// 팝업 생성 시 스크롤바 제거, 팝업 제거시 다시 스크롤바 생성
 
 function Popup({ setOpen }) {
-	const [Num, setNum] = useState(0);
+	let [Num, setNum] = useState(0);
+
+	// 의존성 배열이 비어 있는 useEffect문 (컴포넌트 생성을 캐치)
+	useEffect(() => {
+		console.log('팝업 컴포넌트 생성');
+		document.body.style.overflow = 'hidden';
+	}, []);
+
+	// 의존성 배열에 특정 state값이 등록되어 있는 useEffect문 (컴포넌트의 특정 state값 변경을 캐치)
+	useEffect(() => {
+		console.log('Num state값 변경');
+	}, [Num]);
+
+	// 의존성 배열이 비어 있는 상태에서 특정 함수가 리턴되는 useEffect문 (컴포넌트의 소멸을 캐치)
+	useEffect(() => {
+		return () => {
+			document.body.style.overflow = 'auto';
+			console.log('컴포넌트 소멸');
+		};
+	}, []);
 
 	const popupStyle = {
 		width: 600,
