f523febdeeb69fe40d7d1ca23fdf6caf6bd1d1e1
carrottechplus <blohzzang@gmail.com>
2023-06-26 10:07:52 +0900

    팝업 초기설정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import Popup from './Popup';
 import './scss/style.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
+const [Open, setOpen] = useState(false);
+
+// 팝업열기 버튼 클릭 시 외부 컴포넌트인 popup 컴포넌트 보이게
+// 팝업 컴포넌트 안쪽에 닫기 버튼을 만들어서 해당 닫기 버튼 클릭시 팝업제거
+// 팝업 컴포넌트 안쪽에 숫자 출력하고 그 밑에 증가, 감소 버튼을 만들어서 해당 버튼 클릭시 숫자가 증가, 감소 출력되도록 처리
 
 function App() {
 	return (
 		<>
-			<FontAwesomeIcon icon={faHouse} />
-			<FontAwesomeIcon icon={faEnvelope} />
-			<FontAwesomeIcon icon={faThumbsUp} />
+			<main>
+				<h1>test</h1>
+				<button>팝업 열기</button>
+				<Popup />
+			</main>
 		</>
 	);
 }
