2a47ae072a6bc7711c586d02179cbf7c223df440
carrottechplus <blohzzang@gmail.com>
2023-06-26 12:09:00 +0900

    회전 버튼 효과 수정

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import './scss/style.scss';
 import { useState, useRef } from 'react';
 
 function App() {
-	const [Num, setNum] = useState(0);
+	let [Num, setNum] = useState(0);
 	// 가상돔 요소가 담길 빈 참조객체를 미리 초기화
 	const article = useRef(null);
 	console.log(article);
 
 	const prev = () => {
-		setNum(Num - 1);
+		// setNum(Num - 1); 한박자 늦음 ? Num이 처음에 0이고 state값이 바뀌면 다음번 사이클때 적용이 되기때문에,,
+		setNum(--Num);
 		article.current.style.transform = `rotate(${45 * Num}deg)`;
 	};
 
 	const next = () => {
-		setNum(Num + 1);
+		// setNum(Num + 1);
+		setNum(++Num);
 		article.current.style.transform = `rotate(${45 * Num}deg)`;
 	};
 
